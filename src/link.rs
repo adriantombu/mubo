1258b4764692c2f83a7f284d1085268998fca98a
Adrian Tombu <adrian@otso.fr>
2023-06-19 13:30:43 +0000

    chore: upgrade toml to latest version

--- src/link.rs
+++ src/link.rs
@@ -5,28 +5,26 @@ use std::collections::HashMap;
 use std::fs;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(bound(deserialize = "'de: 'a"))]
-pub struct Links<'a> {
-    pub links: Vec<Link<'a>>,
+pub struct Links {
+    pub links: Vec<Link>,
 }
 
-impl Links<'_> {
+impl Links {
     pub fn new(data: &str) -> Result<Links> {
         toml::from_str::<Links>(data).context("Failed to deserialize the config.toml file")
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
-#[serde(bound(deserialize = "'de: 'a"))]
-pub struct Link<'a> {
-    pub key: &'a str,
-    pub url: &'a str,
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Link {
+    pub key: String,
+    pub url: String,
 }
 
-impl Link<'_> {
+impl Link {
     pub fn render(&self, tt: &Template, build_path: &str) -> Result<()> {
         let mut context = HashMap::new();
-        context.insert("url", self.url);
+        context.insert("url", self.url.as_str());
 
         let html = tt.render("link", &context)?;
 
